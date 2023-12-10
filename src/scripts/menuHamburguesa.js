469974646479ed8985e559499013617b398a3890
Roger Sancho <rogersanchom@gmail.com>
2023-12-10 17:00:14 +0100

    Add theme selector, fix view transitions scripts

--- src/scripts/menuHamburguesa.js
+++ src/scripts/menuHamburguesa.js
@@ -1,7 +1,6 @@
-export function handleMenuClick() {
+document.addEventListener("astro:page-load", () => {
   document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".hamburger").classList.toggle("expanded");
     document.querySelector(".nav-links").classList.toggle("expanded");
-    console.log("miau");
   });
-}
+});
