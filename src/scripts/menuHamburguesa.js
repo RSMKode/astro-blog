0e308c1bd895e71bb61b441ad2a836e0e0cd9c15
Roger Sancho <rogersanchom@gmail.com>
2023-12-10 10:16:59 +0100

    Fix ViewTransitions script

--- src/scripts/menuHamburguesa.js
+++ src/scripts/menuHamburguesa.js
@@ -1,5 +1,7 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".hamburger").classList.toggle("expanded");
-  document.querySelector(".nav-links").classList.toggle("expanded");
-});
-console.log("miau");
+export function handleMenuClick() {
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".hamburger").classList.toggle("expanded");
+    document.querySelector(".nav-links").classList.toggle("expanded");
+    console.log("miau");
+  });
+}
